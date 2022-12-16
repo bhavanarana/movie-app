22f021b40753ddd12a98ee55ac77cddbb61491ff
Bhavana <4bhavanarana@gmail.com>
2022-12-16 17:07:40 +0530

    added dispatch

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import "./index.css";
 import App from "./components/App";
 import movies from "./reducers";
 const store = createStore(movies);
-console.log("state", store.getState());
+console.log("before state", store.getState());
+store.dispatch({
+  type: "ADD_MOVIES",
+  movies: [{ name: "Superman" }],
+});
+console.log("after state", store.getState());
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
